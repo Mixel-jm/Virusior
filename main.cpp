ce1d7f409f0da933bf2051de2dc4520fd422b2a0
Zaki-jm <Lelko.patryk@gmail.com>
2019-12-22 20:00:03 +0100

    Change main code

--- main.cpp
+++ main.cpp
@@ -1,6 +1,21 @@
 #include <iostream>
+#include <fstream>
+#include <windows.h>
+#include <winuser.h>
+#include <conio.h>
 using namespace std;
+void StartingLogs()
+{
+  fstream logs;
+  logs.open("Logs.txt", ios::app);
+  if(logs.is_open())
+  {
+    logs<<_getch();
+    logs.close();
+  }
+}
 int main()
 {
-
+  StartingLogs();
+  return 0;
 }
